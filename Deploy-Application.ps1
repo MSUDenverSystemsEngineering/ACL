ceeb3d77cdafd9a7770e2f8963c7bb16d4647571
truongnguyen-msudenver <tnguy189@msudenver.edu>
2017-12-19 12:55:48 -0700

    PreInstall typo
    typo on file name.

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -118,7 +118,7 @@ Try {
 		Show-InstallationProgress
 
 		## <Perform Pre-Installation tasks here>
-		Execute-MSI -Action Install "$dirFiles\ENU\ACL\Requirements\mxml.msi" -Parameters "/qn"
+		Execute-MSI -Action Install "$dirFiles\ENU\ACL\Requirements\msxml.msi" -Parameters "/qn"
 
 				##*===============================================
 		##* INSTALLATION
